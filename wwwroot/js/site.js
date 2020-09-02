04a2869d8eebe5a273f7d2109b3f551a1267a529
Pablo Ferreyra <pabloeferreyra@gmail.com>
2020-09-02 09:32:29 -0300

    Update site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,14 +3,25 @@
     var dd = tdate.getDate(); //yields day
     var MM = tdate.getMonth() + 1; //yields month
     var yyyy = tdate.getFullYear(); //yields year
-    var time = tdate.toLocaleTimeString();
+    var h = tdate.getHours();
+    var m = tdate.getMinutes();
+    if (h < 10) {
+        h = '0' + h;
+    }
+
+    if (m < 10) {
+        m = '0' + m;
+    }
+
     if (dd < 10) {
         dd = '0' + dd;
     }
+
     if (MM < 10) {
         MM = '0' + MM;
     }
     var currentDate = yyyy + "-" + MM + "-" + dd;
+    var time = h + ":" + m;
 
 //-----------------------------------------------------  turnos  -----------------------------------------------------//
     $("#clientName").blur(function () {
