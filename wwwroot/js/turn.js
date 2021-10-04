abb5aa07a70127ea8dce4402a8423035f1dd3665
Pablo Ferreyra <pabloeferreyra@gmail.com>
2021-10-03 21:41:19 -0300

    habilitacion de creacion para medico y timeout para actualizacion

--- wwwroot/js/turn.js
+++ wwwroot/js/turn.js
@@ -22,7 +22,28 @@
     }
     var currentDate = yyyy + "-" + MM + "-" + dd;
     var time = h + ":" + m;
+    idleTimer();
 });
+
+function idleTimer() {
+    var t;
+    window.onload = resetTimer;
+    window.onmousemove = resetTimer; // catches mouse movements
+    window.onmousedown = resetTimer; // catches mouse movements
+    window.onclick = resetTimer;     // catches mouse clicks
+    window.onscroll = resetTimer;    // catches scrolling
+    window.onkeypress = resetTimer;  //catches keyboard actions
+
+    function reload() {
+        window.location = self.location.href;  //Reloads the current page
+    }
+
+    function resetTimer() {
+        clearTimeout(t);
+        t = setTimeout(reload, 30000);  // time is in milliseconds (1000 is 1 second)
+    }
+}
+
 
 function ConfirmAccess(uniqueId, isAccessedClicked) {
     var accessedSpan = 'accessedSpan_' + uniqueId;
