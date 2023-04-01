50076e6da93838bef8de82cdccb80f9bf3f03c8d
Pablo Ferreyra <pabloeferreyra@gmail.com>
2023-03-31 23:58:37 -0300

    Fixed Edit Turns and navbar on responsive

--- wwwroot/js/editTurn.js
+++ wwwroot/js/editTurn.js
@@ -42,61 +42,91 @@ $(document).ready(function () {
     }
     currentDate = dd + "/" + MM + "/" + yyyy;
     time = h + ":" + m;
-
-    //-----------------------------------------------------  turnos  -----------------------------------------------------//
-    $("#clientName").blur(function () {
-        if ($("#clientName").val() == '') {
-            $("#clientValidation").text('Requerido');
-            Swal.fire({
-                position: 'top-end',
-                icon: 'info',
-                title: 'Por favor ingrese nombre de cliente.',
-                showConfirmButton: false,
-                timer: 600
-            });
-            $("#btnCrearTurno").prop('disabled', true);
-        }
-        else {
-            $("#clientValidation").text('');
-            $("#btnCrearTurno").prop('disabled', false);
-        }
-    });
-
-    $("#dniCliente").blur(function () {
-        if ($("#dniCliente").val().length < 6) {
-            $("#dniValidation").text('El DNI debe tener por lo menos 6 (seis) caracteres.');
-            Swal.fire({
-                position: 'top-end',
-                icon: 'info',
-                title: 'El DNI debe tener por lo menos 6 (seis) caracteres.',
-                showConfirmButton: false,
-                timer: 600
-            });
-            $("#btnCrearTurno").prop('disabled', true);
-        }
-        else {
-            $("#clientValidation").text('');
-            $("#dniValidation").text('');
-            $("#btnCrearTurno").prop('disabled', false);
-        }
-    });
-
-    $("#timeTurn").blur(function () {
-        if ($("#timeTurn :selected").text() <= time && ($("#DateTurnEdit").val() == currentDate)) {
-            $("#timeValidation").text('Requerido.');
-            Swal.fire({
-                position: 'top-end',
-                icon: 'info',
-                title: 'La hora no puede ser anterior.',
-                showConfirmButton: false,
-                timer: 600
-            });
-            $("#btnCrearTurno").prop('disabled', true);
-        }
-        else {
-            $("#timeValidation").text('');
-        }
-    });
-    //-----------------------------------------------------  turnos  -----------------------------------------------------//
-
 });
+//-----------------------------------------------------  turnos  -----------------------------------------------------//
+$("#clientName").blur(function () {
+    if ($("#clientName").val() == '') {
+        $("#clientValidation").text('Requerido');
+        Swal.fire({
+            position: 'top-end',
+            icon: 'info',
+            title: 'Por favor ingrese nombre de cliente.',
+            showConfirmButton: false,
+            timer: 600
+        });
+        $("#btnCrearTurno").prop('disabled', true);
+    }
+    else {
+        $("#clientValidation").text('');
+        $("#btnCrearTurno").prop('disabled', false);
+    }
+});
+
+$("#dniCliente").blur(function () {
+    if ($("#dniCliente").val().length < 6) {
+        $("#dniValidation").text('El DNI debe tener por lo menos 6 (seis) caracteres.');
+        Swal.fire({
+            position: 'top-end',
+            icon: 'info',
+            title: 'El DNI debe tener por lo menos 6 (seis) caracteres.',
+            showConfirmButton: false,
+            timer: 600
+        });
+        $("#btnCrearTurno").prop('disabled', true);
+    }
+    else {
+        $("#clientValidation").text('');
+        $("#dniValidation").text('');
+        $("#btnCrearTurno").prop('disabled', false);
+    }
+});
+
+$("#timeTurn").blur(function () {
+    if ($("#timeTurn :selected").text() <= time && ($("#DateTurnEdit").val() == currentDate)) {
+        $("#timeValidation").text('Requerido.');
+        Swal.fire({
+            position: 'top-end',
+            icon: 'info',
+            title: 'La hora no puede ser anterior.',
+            showConfirmButton: false,
+            timer: 600
+        });
+        $("#btnCrearTurno").prop('disabled', true);
+    }
+    else {
+        $("#timeValidation").text('');
+    }
+});
+//-----------------------------------------------------  turnos  -----------------------------------------------------//
+
+
+$("#btnEditarTurno").on('click', function (event) {
+    event.preventDefault();
+    EditTurn();
+});
+
+
+function EditTurn() {
+    var form = $('#__AjaxAntiForgeryForm');
+    var token = $('input[name="__RequestVerificationToken"]', form).val();
+    let formData = $("#EditForm").serialize();
+    console.log(formData);
+    $.ajax({
+        type: "PUT",
+        url: "/Turns/Edit",
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+        header: token,
+        data: formData,
+        success: function () {
+            $("#Edit").modal('toggle');
+            reset();
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'Turno editado correctamente.',
+                showConfirmButton: false,
+                timer: 600
+            });
+        },
+    });
+}
