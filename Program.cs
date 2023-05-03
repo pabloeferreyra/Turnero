d9f84f49d31500718f95538ab14c0ec1bdbd3691
Pablo Ferreyra <pabloeferreyra@gmail.com>
2023-05-03 10:02:47 -0300

    still isn't working

--- Program.cs
+++ Program.cs
@@ -33,6 +33,7 @@ if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
 else
 {
     secretsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".microsoft", "UserSecrets", builder.Configuration["secretsFolder"], "secrets.json");
+    Console.WriteLine(secretsPath);
 }
 
 #endregion
@@ -44,7 +45,6 @@ builder.Configuration.AddUserSecrets<Program>();
 #endregion
 
 var connectionString = builder.Configuration["ConnectionStrings:SQLite"];
-Console.WriteLine(connectionString);
  builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString)).AddDefaultIdentity<IdentityUser>(options =>
 {
