20098e9cb411192d9e89d2f523130f3d7453f42c
Pablo Ferreyra <pabloeferreyra@gmail.com>
2024-12-16 11:58:38 -0300

    idk what happened

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ builder.Configuration.AddJsonFile(secretsPath, optional: true);
 builder.Configuration.AddUserSecrets<Program>();
 #endregion
 
-var connectionString = builder.Configuration["ConnectionStrings:PostgresDemoConnection"];
+AppSettings.ConnectionString = builder.Configuration["ConnectionStrings:PostgresConnection"];
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
    options.UseNpgsql(AppSettings.ConnectionString)).AddDefaultIdentity<IdentityUser>(options =>
    {
