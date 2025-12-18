381b5aecf31ef2280fd6a938a50ff1630de44f59
Pablo Ferreyra <pabloeferreyra@gmail.com>
2025-12-18 16:09:59 -0300

    Change database connection string to Postgres

--- Program.cs
+++ Program.cs
@@ -40,7 +40,7 @@ if (builder.Environment.IsDevelopment())
 #endregion
 
 #region Database Configuration
-AppSettings.ConnectionString = builder.Configuration.GetConnectionString("LocalConnection");
+AppSettings.ConnectionString = builder.Configuration.GetConnectionString("PostgresConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(AppSettings.ConnectionString));
 
